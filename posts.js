cbd587eaab2be8b848efe36a0343ab4056af2712
saidhanyan <tempestsecondary@gmail.com>
2026-07-06 18:41:25 +0530

    Update posts.js

--- posts.js
+++ posts.js
@@ -42,4 +42,36 @@ That's really the whole idea behind this site. One column, one voice, nothing co
 `
   },
 
+   {
+      slug: "smoke-and-mirrors-of-the-self",
+      title: "Smoke & Mirrors of the Self",
+      date: "2026-07-06",
+      content: `
+   Confession is by one of the best relievers I've experienced in life. It's an act of relieving yourself from carrying and maintaining a synthesized image that even in the best cases a partially true image.
+
+Carrying and maintaining a synthesized image comes with a cost of peace and expression because behind the image, you will be a different person than the one your image says that you are. So, you can't say things you want to say and you can't act like you want to act, that is where expression is cut off and whenever expression is cut off, peace is cut off too. You can see this all scales. On a personal scale, it can take forms as anxiety and depression and on a societal scale, it can take forms as protest and revolutions, historically speaking.
+
+So, what is with the title "Smoke & Mirrors of the Self"?
+
+Smoke and Mirrors are everything that are used to present a partial truth to hide the full truth because the full truth doesn't seem to be attractive. It's an idiom named after a famous magic trick which used mirrors and smoke to create an illusion.
+
+The images we create about ourselves are exactly like Smoke & Mirrors that deceive and distract not only others but also us from the truth because we forget truth when we repeat lies.
+
+As a result, whenever we maintain an image, our actions and our opinions will always feel less authentic and distant from well thought because they will not be a product of our authentic self.
+
+Personally, confession has helped me beyond metrics. I'll share the exact moment when I found out that confession is instrumental to our lives.
+
+When I was a freshman (2022) in college, I committed a morally grave mistake that after doing that I felt absolutely horrible that I never felt sooooo bad.
+
+The all-good and virtuous image I had about myself was broken the moment I realized I committed a morally grave mistake. I started to have deep self-doubts and felt like an impostor especially when my family was around me.
+
+It was there for a few weeks, then one day I said to myself that it's enough, called my mom, broke the bad news to her and asked if her love for me diminishes due to this. It was this feeling that ruled me, the feeling that "I'm not the person who people know and love me as, would they hate me if they find out?".
+
+But her reply was "How can I be angry, son? I'm just scared if you'd derail in life... You just have to change yourselves and don't worry, we are by your side". That's when I dropped tonnes of weight off me and felt like a feather. Specially because I didn't have to maintain an artificial image anymore and that one of the most important people in my life know me as I am and still loves me.
+
+And that's how confession helped me. In my case, I knew I had a parent who will listen to me and not judge me, so I chose to confess to them. This might not be the case for everyone, so make sure you assess the person before confessing to them. 
+
+`
+   },
+
 ];
