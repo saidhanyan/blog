7c5afd29ac0490e9d7fefbacb214a26e09d86ae0
saidhanyan <tempestsecondary@gmail.com>
2026-07-07 13:13:12 +0530

    Update posts.js

--- posts.js
+++ posts.js
@@ -34,13 +34,13 @@ const POSTS = [
       content: `
 Confession is by far one of the best relievers I've experienced in life. I'd define it as **"an act of relieving yourself from carrying and maintaining a synthesized image that even in the best cases a partially true image"**.
 
-Carrying and maintaining a synthesized image comes with a cost of **peace** and **expression** because behind the image, you will be a different person than the one your image says that you are. So, you can't say things you want to say and you can't act like you want to act, that is where expression is cut off and whenever expression is cut off, peace is cut off too. You can see this in all scales. On a personal scale, lack of expression can take forms as anxiety and depression and on a societal scale, it can take forms as protests and revolutions, historically speaking.
+Carrying and maintaining a synthesized image comes with a cost of **peace** and **expression** because behind the image, you will be a different person than the one your image says that you are. So, **you can't say things you want to say** and **you can't act like you want to act**, that is where expression is cut off and whenever expression is cut off, peace is cut off too. You can see this in all scales. On a personal scale, lack of expression can take forms as anxiety and depression and on a societal scale, it can take forms as protests and revolutions, historically speaking.
 
 So, what is with the title **"Smoke & Mirrors of the Self"**?
 
-Smoke and Mirrors are everything that are used to present a partial truth to hide the full truth because the full truth doesn't seem to be attractive. It's an idiom named after a famous magic trick which used mirrors and smoke to create an illusion.
+Smoke and Mirrors is everything that is used to present a partial truth to hide the full truth because the full truth doesn't seem to be attractive. It's an idiom named after a famous magic trick which used mirrors and smoke to create an illusion.
 
-The images we create about ourselves are exactly like Smoke & Mirrors that deceive and distract not only others but also us from the truth because we forget truth when we repeat lies.
+The images we create about ourselves are exactly like Smoke & Mirrors that deceive and distract not only others but also us from the truth because we forget the truth when we repeat lies.
 
 As a result, whenever we maintain an image, our actions and our opinions will always feel less authentic and distant from well thought **because they will not be a product of our authentic self**.
 
@@ -48,23 +48,23 @@ Personally, confession has helped me beyond metrics. I'll share the exact moment
 
 ## THE CONFESSION
 
-When I was a freshman (2022) in college, I committed a morally grave mistake that after doing which I felt absolutely horrible that I never felt sooooo bad.
+When I was a freshman (2022) in college, I committed a morally grave mistake, after which I felt absolutely horrible that I never felt sooooo bad.
 
-The all-good and virtuous image I had about myself was broken the moment I realized I committed a morally grave mistake. I started to have deep self-doubts and felt like an impostor especially when my family was around me.
+The all-good and virtuous image I had about myself was broken the moment I realized I had committed a morally grave mistake. I started to have deep self-doubts and felt like an impostor especially when my family was around me.
 
-It was there for a few weeks, then one day I said to myself that it's enough, called my mom, broke the bad news to her and asked if her love for me diminishes due to this. It was this feeling that ruled me, the feeling that "I'm not the person who people know and love me as, would they hate me if they find out?".
+It was there for a few weeks, then one day I said to myself that it's enough, called my mom, broke the bad news to her and asked if her love for me diminishes due to this. It was this feeling that ruled me, the feeling that "I'm not the person who people know and love me as, would they hate me if they find out?"
 
-But her reply was "How can I be angry, son? I'm just scared if you'd derail in life... You just have to change yourself and don't worry, we are by your side". That's when I dropped tonnes of weight off me and felt like a feather. Especially because I didn't have to maintain an artificial image anymore and that one of the most important people in my life know me as I am and still loves me.
+But her reply was "How can I be angry, son? I'm just scared if you'd derail in life... You just have to change yourself and don't worry, we are by your side". That's when I dropped tonnes of weight off me and felt like a feather. Especially because I didn't have to maintain an artificial image anymore and that one of the most important people in my life knows me as I am and still loves me.
 
 ## THE CAUTION
 
-And that's how confession has helped me. In my case, I knew I had a parent who will listen to me and not judge me, so I chose to confess to them. This might not be the case for everyone, so make sure you assess the person before confessing to them. For example, say that my mom reacted differently to it out of anger, then it would have become problem plus one for me from then on, whenever she sees me, I'll feel more guilt than I did before. 
+And that's how confession has helped me. In my case, I knew I had a parent who would listen to me and not judge me, so I chose to confess to them. This might not be the case for everyone, so make sure you assess the person before confessing to them. For example, say that my mom reacted differently to it out of anger, then it would have become problem plus one for me from then on, whenever she sees me, I'll feel more guilt than I did before. 
 
 **The goal is to reduce the chaos within**, for which you are going to rely on another person, if doing that is going to introduce chaos without, you will still get the part relief of confession but at the cost of a morphed relationship with the person you have confessed to and the chaos that it causes.
 
 ## EXTRA
 
-Smoke + Mirrors is an album by **Imagine Dragons** whose title song is "Smoke and Mirrors". This song perfectly captures one's inner turmoil when they are suffering with their images. I'll just drop the song's chorus here, you can listen the song to get the feel.
+Smoke + Mirrors is an album by **Imagine Dragons** whose title song is "Smoke and Mirrors". This song perfectly captures one's inner turmoil when they are suffering with their images. I'll just drop the song's chorus here, you can listen to the song to get the feel.
 
 ## Chorus:
 
@@ -73,7 +73,6 @@ Smoke + Mirrors is an album by **Imagine Dragons** whose title song is "Smoke an
 > All that I hope, is it just smoke and mirrors?
 > I wanna believe, oh
 > But all that I hope, is it just smoke and mirrors?
-
 `
    },
 
