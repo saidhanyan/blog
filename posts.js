49fa9928311eede6167cca5330966b9641a468dd
saidhanyan <tempestsecondary@gmail.com>
2026-07-17 10:34:29 +0530

    Update posts.js

--- posts.js
+++ posts.js
@@ -32,7 +32,7 @@ const POSTS = [
       title: "Smoke & Mirrors of the Self",
       date: "2026-07-06",
       content: `
-Confession is by far one of the best relievers I've experienced in life. I'd define it as **"an act of relieving yourself from carrying and maintaining a synthesized image that even in the best cases a partially true image"**.
+Confession is by far one of the best relievers I've experienced in life. I'd define it as **"an act of relieving yourself from carrying and maintaining a synthesized image that even in the best cases is a partially true image"**.
 
 Carrying and maintaining a synthesized image comes with a cost of **peace** and **expression** because behind the image, you will be a different person than the one your image says that you are. So, **you can't say things you want to say** and **you can't act like you want to act**, that is where expression is cut off and whenever expression is cut off, peace is cut off too. You can see this in all scales. On a personal scale, lack of expression can take forms as anxiety and depression and on a societal scale, it can take forms as protests and revolutions, historically speaking.
 
@@ -42,7 +42,7 @@ Smoke and Mirrors is everything that is used to present a partial truth to hide 
 
 The images we create about ourselves are exactly like Smoke & Mirrors that deceive and distract not only others but also us from the truth because we forget the truth when we repeat lies.
 
-As a result, whenever we maintain an image, our actions and our opinions will always feel less authentic and distant from well thought **because they will not be a product of our authentic self**.
+As a result, whenever we maintain an image, our actions and our opinions will always feel less authentic and distant from well-thought **because they will not be a product of our authentic self**.
 
 Personally, confession has helped me beyond metrics. I'll share the exact moment when I found out that confession is instrumental to our lives.
 
@@ -50,7 +50,7 @@ Personally, confession has helped me beyond metrics. I'll share the exact moment
 
 When I was a freshman (2022) in college, I committed a morally grave mistake, after which I felt absolutely horrible that I never felt sooooo bad.
 
-The all-good and virtuous image I had about myself was broken the moment I realized I had committed a morally grave mistake. I started to have deep self-doubts and felt like an impostor especially when my family was around me.
+The all-good and virtuous image I had about myself was broken the moment I realized I had committed a morally grave mistake. I started to have deep self-doubts and felt like an impostor, especially when my family was around me.
 
 It was there for a few weeks, then one day I said to myself that it's enough, called my mom, broke the bad news to her and asked if her love for me diminishes due to this. It was this feeling that ruled me, the feeling that "I'm not the person who people know and love me as, would they hate me if they find out?"
 
@@ -58,33 +58,33 @@ But her reply was "How can I be angry, son? I'm just scared if you'd derail in l
 
 ## THE CAUTION
 
-And that's how confession has helped me. In my case, I knew I had a parent who would listen to me and not judge me, so I chose to confess to them. This might not be the case for everyone, so make sure you assess the person before confessing to them. For example, say that my mom reacted differently to it out of anger, then it would have become problem plus one for me from then on, whenever she sees me, I'll feel more guilt than I did before. 
+And that's how confession has helped me. In my case, I knew I had a parent who would listen to me and not judge me, so I chose to confess to them. This might not be the case for everyone, so make sure you assess the person before confessing to them. For example, say that my mom reacted differently to it out of anger, then it would have become a problem plus one for me from then on, whenever she sees me, I'll feel more guilt than I did before. 
 
 **The goal is to reduce the chaos within**, for which you are going to rely on another person, if doing that is going to introduce chaos without, you will still get the part relief of confession but at the cost of a morphed relationship with the person you have confessed to and the chaos that it causes.
 
 ## ASSESS A PERSON? HOW?
 
-One of the draft readers for this essay asked me **"You say assess a person but how do I do it?"**. At the top of my mind, I didn't know the answer to it. 
+One of the draft readers for this essay asked me, **"You say assess a person but how do I do it?"**. At the top of my mind, I didn't know the answer to it. 
 
-It's hard. Nobody is consciously assessing other people unless and until they are expecting a serious relationship between them (or are we?). So, I tried to work backward to reason why my mom seemed so credible to confess to in order to find the answers to that question. And, for myself, these came out to be the answers, for another person, the answers can be different:
+It's hard. Nobody is consciously assessing other people unless and until they are expecting a serious relationship between them (or are we?). So, I tried to work backward to reason why my mom seemed so credible to confess to in order to find the answers to that question. And, for myself, these came out to be the answers; for another person, the answers can be different:
 
-> Unless and until a person had spent a lot of time with another person, they cannot have a proper assessment about the other person.
+> Unless and until a person has spent a lot of time with another person, they cannot have a proper assessment of the other person.
 
-> Unless and until a person has experienced a variety of emotions with another person, they cannot have a proper assessment about the person.
+> Unless and until a person has experienced a variety of emotions with another person, they cannot have a proper assessment of the person.
 
 So, maybe we can have these as *thumb rules* to see if we have an assessment about some people we know in life. So, if I feel the need to confess to someone, I would ask myself..
 
 **1. Had I spent a lot of time with them?**
-I lived almost my entire life with my mom, it's only for college I was away from home. Generally put, she is one of the people with whom I had spent a lot of time.
+I lived almost my entire life with my mom. It's only for college that I was away from home. Generally put, she is one of the people with whom I have spent a lot of time.
 
 **2. Had I experienced a variety of emotions with them?** 
 Since she is family, like any family, we went through all kinds of hardships and times of happiness together. So I was able to see her react to situations all throughout my life and based on that, I know she will listen to me.
 
 And throughout that much time and companionship, if they had proved to be credible to me, I would go on to confess.
 
-These are my filters, if you spend some time thinking what could be those filters for you, you will definitely have some.
+These are my filters, if you spend some time thinking about what those filters could be for you, you will definitely have some.
 
-Then, if you apply those filters and hit search, the results will show you some people in your life and if not, I think that's when people resort to Reddit's r/confession subreddits, *absolutely no pun intended*, you can see for yourself the amount of people confessing online.
+Then, if you apply those filters and hit search, the results will show you some people in your life and if not, I think that's when people resort to Reddit's r/confession subreddits, *absolutely no pun intended*, you can see for yourself the number of people confessing online.
 
 ## EXTRA
 
